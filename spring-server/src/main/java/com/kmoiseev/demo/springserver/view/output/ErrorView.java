aeed317806bc6ce47b8f8893ee151442da2e4433
Konstantin Moiseev <konstantin.moiseev@mail.ru>
2020-02-05 14:56:24 +0300

    Initial set of changes for integration tests

--- spring-server/src/main/java/com/kmoiseev/demo/springserver/view/output/ErrorView.java
+++ spring-server/src/main/java/com/kmoiseev/demo/springserver/view/output/ErrorView.java
@@ -2,8 +2,10 @@ package com.kmoiseev.demo.springserver.view.output;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor(access = AccessLevel.PUBLIC, staticName = "of")
+@Getter
 public class ErrorView {
     private final String error;
 }
